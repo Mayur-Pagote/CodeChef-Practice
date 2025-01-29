6b11c9e29154445f52348604c9f88d71cc60c306
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-01-29 22:30:24 +0530

    Create Advitiya.py

---
+++ Advitiya.py
@@ -0,0 +1,13 @@
+# cook your dish here
+
+user_input = int(input())
+    
+for i in range(user_input):
+    ms = "ADVITIYA" 
+    count = 0
+    string = input().strip()
+    for j in range (len(string)):
+        count += (ord(ms[j]) - ord(string[j]) + 26) % 26
+    print(count)
+    
+    
