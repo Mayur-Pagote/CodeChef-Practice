a403875e86939c2e7b03d5d913b0d3b373a76556
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-05-03 17:17:25 +0530

    Create Debug the code - 7.java

---
+++ Debug the code - 7.java
@@ -0,0 +1,18 @@
+// Debug the code
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		char name = 'A';
+		if(name=='A' || name =='C'){
+		    System.out.println(1);
+		}
+		else{
+		    System.out.println(0);
+		}
+
+	}
+}
