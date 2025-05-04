ede68fd690ecfeb8b2ddaf662569df4f984e9f24
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-05-04 21:19:46 +0530

    Create Debug the code - 8.java

---
+++ Debug the code - 8.java
@@ -0,0 +1,18 @@
+// Debug the given code
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    int age = 18;
+	    if(age >= 18){
+	        System.out.println(1);
+	    }
+	    else{
+	        System.out.println(0);
+	    }
+	}
+}
