f9a17b2130de42503448eae11626177ff992d9aa
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-05-07 11:33:08 +0530

    Create Debug the code - 11.java

---
+++ Debug the code - 11.java
@@ -0,0 +1,16 @@
+// Debug the given code
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        int s = 14;
+        int area = s * s;
+        double cost = area * 7;
+        System.out.println(area);
+        System.out.println((int)cost + "$");
+	}
+}
