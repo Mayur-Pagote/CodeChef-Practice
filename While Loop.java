3b9a7fbbe839cb988ebd800a98ee1f2b9591464e
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-05-09 11:00:36 +0530

    Create While Loop.java

---
+++ While Loop.java
@@ -0,0 +1,12 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		int a = 0;
+        // Write the loop here
+        while (a < 7){
+            System.out.println(a);
+            a += 1;
+        }
+	}
+}
