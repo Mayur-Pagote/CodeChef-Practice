ffebc3eaf1b88fd0f6dfe896cf09bd95cd49a9fd
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-05-13 17:32:03 +0530

    Create User input Loop.java

---
+++ User input Loop.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		Scanner read = new Scanner(System.in);
+		int num = read.nextInt();
+		int a = 0;
+		// Update your code below this line
+		for (int i = 0; i<num; i++){
+		    System.out.println(i);
+		}
+
+
+	}
+}
