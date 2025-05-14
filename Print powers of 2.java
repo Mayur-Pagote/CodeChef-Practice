e632e54f093f018d9a98b3729a3477c7f62e4125
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-05-14 07:58:54 +0530

    Create Print powers of 2.java

---
+++ Print powers of 2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		int i = 2;
+		while (i <= 128){
+		    System.out.println(i);
+		    i *= 2;
+		}
+
+	}
+}
