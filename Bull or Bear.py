97ff465e5a142965f498bb77a7f5353335a6b373
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-09-16 06:23:33 +0530

    Create Bull or Bear.py

---
+++ Bull or Bear.py
@@ -0,0 +1,10 @@
+# cook your dish here
+
+for i in range(int(input())):
+    x, y =map(int, input().split())
+    if x == y:
+        print("NEUTRAL")
+    elif x < y:
+        print("PROFIT")
+    else:
+        print("LOSS")
