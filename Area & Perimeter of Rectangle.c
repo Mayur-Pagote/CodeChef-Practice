26e6a2820703db3ffb0298d1cb5f321393ba1251
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-12-08 08:43:48 +0530

    Update Area & Perimeter of Rectangle.c

--- Area & Perimeter of Rectangle.c
+++ Area & Perimeter of Rectangle.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    
-    // Add the formula for area of 11 and 13
+
 	printf("%d\n", 11*13);
 	
-	// Add the formula for perimeter of 11 and 13
 	printf("%d\n", 2 * (11 + 13));
 }
 
