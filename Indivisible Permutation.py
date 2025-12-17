4ac497641573416ec0601dd5be5724f83ec6bbcc
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-12-17 18:20:03 +0530

    Create Indivisible Permutation.py

---
+++ Indivisible Permutation.py
@@ -0,0 +1,15 @@
+# cook your dish here
+
+
+
+for i in range(int(input())):
+    l = []
+    for j in range(2, int(input())+1):
+        l.append(j)
+        
+    l.append(1)
+    
+    
+    for k in l:
+        print(k, end=" ")
+    print()
