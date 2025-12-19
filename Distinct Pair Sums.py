49f616d6ec1e86a8438d503858bcfbe0d81a94d9
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-12-19 10:58:04 +0530

    Update Distinct Pair Sums.py

--- Distinct Pair Sums.py
+++ Distinct Pair Sums.py
@@ -4,3 +4,16 @@ for i in range(int(input())):
     L,R = map(int, input().split())
     res = 2*(R-L)+1
     print(res)
+
+
+# Second sol
+# cook your dish here
+
+for i in range(int(input())):
+    x,y = map(int, input().split())
+    count = 0
+    if x == y:
+        print(1)
+    else:
+        
+        print((2*y+1) - (2*x))
