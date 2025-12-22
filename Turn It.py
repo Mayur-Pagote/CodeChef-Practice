3449ebc827e4d7c60e8b745aedf0e5544cf9e553
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-12-22 10:17:20 +0530

    Update Turn It.py

--- Turn It.py
+++ Turn It.py
@@ -1,6 +1,12 @@
+# cook your dish here
+
+import math
 for i in range(int(input())):
     U,V,A,S = map(int, input().split())
-    if (U * U) - (2*A*S) <= (V * V):
-        print("Yes")
+    newton = U*U - (2*A*S)
+    if newton <= 0:
+        print("YES")
+    elif newton**0.5 <= V:
+        print("YES")
     else:
-        print("No")
+        print("NO")
